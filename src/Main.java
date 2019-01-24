0946718b8fa6af8d0dd7ec0f5589d50c6a565d14
dominik.rys@hotmail.com <dominik.rys@hotmail.com>
2019-01-24 14:10:35 +0000

    Cleaned up imports and formatted

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,16 @@
 import data.GameState;
 import data.Location;
 import data.Pose;
-import data.enemy.Enemy;
 import data.enemy.Zombie;
-import data.item.Item;
-import data.item.ItemList;
 import data.item.weapon.Pistol;
 import data.map.Meadow;
 import data.player.Player;
-import data.projectile.Projectile;
 import data.projectile.SmallBullet;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Label;
-import javafx.scene.effect.BlurType;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.image.*;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 
 import java.util.LinkedHashSet;
 import java.util.Optional;
-import java.util.Stack;
 
 public class Main extends Application {
     @Override
