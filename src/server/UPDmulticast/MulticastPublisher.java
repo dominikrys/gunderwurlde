d70b7fcc761e6482180bab7ec03dc8a018ad8ed8
Tomvyc <tomas.vycas111@gmail.com>
2019-01-30 16:40:06 +0000

    tests

--- src/server/UPDmulticast/MulticastPublisher.java
+++ src/server/UPDmulticast/MulticastPublisher.java
@@ -11,22 +11,21 @@ public class MulticastPublisher {
     private static final String MULTICAST_INTERFACE = "eth0";
     private static final int MULTICAST_PORT = 4321;
     private static final String MULTICAST_IP = "230.0.0.0";
-    public void sendMessage(String ip, String iface, int port,
-                            String message) throws IOException {
+    public void sendMessage(String ip, String iface, int port, String message) throws IOException {
+
         DatagramChannel datagramChannel=DatagramChannel.open();
         datagramChannel.bind(null);
-        NetworkInterface networkInterface=NetworkInterface
-                .getByName(iface);
-        datagramChannel.setOption(StandardSocketOptions
-                .IP_MULTICAST_IF,networkInterface);
-        ByteBuffer byteBuffer=ByteBuffer.wrap
-                (message.getBytes());
-        InetSocketAddress inetSocketAddress=new
-                InetSocketAddress(ip,port);
-        datagramChannel.send(byteBuffer,inetSocketAddress);
+        NetworkInterface networkInterface=NetworkInterface.getByName(iface);
+
+        datagramChannel.setOption(StandardSocketOptions.IP_MULTICAST_IF,networkInterface);
+
+        ByteBuffer byteBuffer=ByteBuffer.wrap(message.getBytes());
+
+        InetSocketAddress inetSocketAddress= new InetSocketAddress(ip, port);
+        datagramChannel.send(byteBuffer, inetSocketAddress);
     }
     public static void main(String[] args) throws IOException {
-        MulticastPublisher mp=new MulticastPublisher();
+        MulticastPublisher mp = new MulticastPublisher();
         mp.sendMessage(MULTICAST_IP,MULTICAST_INTERFACE,
                 MULTICAST_PORT,"Hi there!");
     }
