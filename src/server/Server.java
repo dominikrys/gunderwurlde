8e2bee5cd2f6060ddf8ec881d212c14f5310d6fa
rxm747 <rxm747@cs.bham.ac.uk>
2019-02-04 18:33:08 +0000

    Some stuff for the Server.

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,9 +1,18 @@
 package server;
 
 import client.data.GameView;
+import data.map.MapList;
 import server.game_engine.HasEngine;
+import server.game_engine.ProcessGameState;
+import server.request.ClientRequests;
 
 public class Server implements HasEngine {
+    protected ClientRequests clientRequests;
+    protected ProcessGameState engine;
+    
+    public Server(MapList mapName, String hostName) {
+        this.engine = new ProcessGameState(this, mapName, hostName);
+    }
 
     @Override
     public void removePlayer(int playerID) {
@@ -15,6 +24,11 @@ public class Server implements HasEngine {
     public void updateGameView(GameView view) {
         // TODO Auto-generated method stub
         
+    }
+
+    @Override
+    public void requestClientRequests() {
+        if (clientRequests != null) engine.setClientRequests(clientRequests);
     }
 
 }
