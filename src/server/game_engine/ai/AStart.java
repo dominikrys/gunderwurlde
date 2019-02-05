7bde7b7aeed2e2ac0f3c721e1d592abbc0d856d9
Tomvyc <tomas.vycas111@gmail.com>
2019-02-05 14:37:36 +0000

    Add removeLeafs for shorter paths

--- src/server/game_engine/ai/AStart.java
+++ src/server/game_engine/ai/AStart.java
@@ -11,42 +11,83 @@ import static java.lang.Math.sqrt;
 public class AStart {
 
     private static final int COST_OF_TRAVEL = 1;
+    private static double[][] realDist;
+    private static final int[][] tiles = {
+            {1, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 1, 0, 0, 0, 1, 1, 0, 0},
+            {0, 0, 1, 0, 0, 1, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 1, 1, 1, 1, 0, 0},
+            {0, 0, 1, 0, 0, 0, 0, 0, 0}
+    };
 
     public static void main(String[] args) {
         //init
-        int[][] tiles = {
-                {1, 0, 0, 0, 0, 0, 1, 0, 0},
-                {0, 1, 0, 0, 0, 1, 1, 0, 0},
-                {0, 0, 1, 0, 0, 1, 1, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 1, 1, 1, 1, 1, 0, 0},
-                {0, 0, 1, 0, 0, 0, 0, 0, 0}
-        };
 
         Pair<Integer, Integer> playerLoc = new Pair<Integer, Integer>(0, 1); //x and y
         Pair<Integer, Integer> enemLoc = new Pair<Integer, Integer>(5, 5);
 
+        realDist = calcRealDist(playerLoc);
 
-        List<Pair<Integer, Integer>> path = aStar(tiles, playerLoc, enemLoc);
+        ArrayList<Pair<Integer, Integer>> path = aStar(enemLoc, playerLoc);
+        path  = removeLeafs(path);
+
+
         System.out.println("\n\n\n");
         for (Pair<Integer, Integer> pair: path) {
             System.out.println(pair);
         }
     }
 
-    private static List<Pair<Integer, Integer>> aStar(
-            int[][] tiles, Pair<Integer, Integer> playerLoc, Pair<Integer, Integer> enemLoc) {
+    private static ArrayList<Pair<Integer, Integer>> removeLeafs(ArrayList<Pair<Integer, Integer>> path){
+        Pair<Integer,Integer> biggestDist = path.get(0);
+        Pair<Integer,Integer> peak;
 
-        double[][] realDist = calcRealDist(tiles, playerLoc);
-        List<Pair<Integer, Integer>> closed = new ArrayList();
+        System.out.println("\n\n");
+
+        for (Pair<Integer, Integer> coord : path) {
+            System.out.println("Coords: " + coord + " Dist to player: " + realDist[coord.getKey()][coord.getValue()]);
+            if(realDist[biggestDist.getKey()][biggestDist.getValue()] < realDist[coord.getKey()][coord.getValue()]){
+                biggestDist = coord;
+            }
+        }
+
+        peak = biggestDist;
+        System.out.println("Peak: " + peak);
+
+        if(peak != path.get(0)){
+            System.out.println("ShortCut: ");
+            realDist = calcRealDist(peak);
+            ArrayList<Pair<Integer, Integer>> shortCut = aStar(path.get(0), peak);
+
+            for (Pair<Integer, Integer> coord : shortCut) {
+                System.out.println(coord);
+            }
+            ArrayList<Pair<Integer, Integer>> shorterPath = new ArrayList<>();
+
+            for (int i = 0; i < shortCut.size(); i++) {
+                shorterPath.add(shortCut.get(i));
+            }
+            shorterPath.addAll(path.subList(path.indexOf(peak) + 1, path.size()));
+
+            return shorterPath;
+        }
+
+
+
+        return path;
+    }
+
+    private static ArrayList<Pair<Integer, Integer>> aStar(Pair<Integer, Integer> startCoords, Pair<Integer, Integer> endCoords) {
+
+        ArrayList<Pair<Integer, Integer>> closed = new ArrayList();
         PriorityQueue<Node> newNodes = null;
-        PriorityQueue<Node> opened = openNodes(tiles, enemLoc, 0d, realDist);
+        PriorityQueue<Node> opened = openNodes(startCoords, 0d);
 
-        closed.add(enemLoc);
+        closed.add(startCoords);
 
-        while(!closed.contains(playerLoc)) {
-            Node nodeToOpen = opened.peek();
-            newNodes = openNodes(tiles, nodeToOpen.getCoordinates(), nodeToOpen.getCostToGo(), realDist);
+        while(!closed.contains(endCoords)) {
+            newNodes = openNodes(opened.peek().getCoordinates(), opened.peek().getCostToGo());
             closed.add(opened.poll().getCoordinates());
 
             for (Node n: newNodes) {
@@ -58,14 +99,14 @@ public class AStart {
         return closed;
     }
 
-    private static double[][] calcRealDist(int[][] tiles, Pair<Integer, Integer> playerLoc) {
+    private static double[][] calcRealDist(Pair<Integer, Integer> endCoords) {
         double[][] realDist = new double[tiles.length][tiles[0].length];
 
         System.out.println("\n\nReal (straight line) distances form every node to player: ");
         for (int i = 0; i < tiles.length; ++i) {
             System.out.println();
             for (int j = 0; j < tiles[i].length; ++j) {
-                realDist[i][j] = sqrt(pow(j - playerLoc.getValue(), 2) + pow(i - playerLoc.getKey(), 2));
+                realDist[i][j] = sqrt(pow(j - endCoords.getValue(), 2) + pow(i - endCoords.getKey(), 2));
                 System.out.print(String.format("%.1f ", realDist[i][j]));
             }
         }
@@ -73,7 +114,7 @@ public class AStart {
         return realDist;
     }
 
-    private static PriorityQueue<Node> openNodes(int[][] tiles, Pair<Integer, Integer> nodeLoc, double costToGo, double[][] realDist) {
+    private static PriorityQueue<Node> openNodes(Pair<Integer, Integer> nodeLoc, double costToGo) {
         PriorityQueue<Node> initNodes = new PriorityQueue<>(8, (Node o1, Node o2) -> {
             if (o1.getSum() < o2.getSum())
                 return -1;
