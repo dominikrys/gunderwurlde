647241d92d27d03dd06faf9d56e23dbef4cf8ea5
Tomvyc <tomas.vycas111@gmail.com>
2019-02-05 15:02:44 +0000

    Add comments for AI* search

--- src/server/game_engine/ai/AStart.java
+++ src/server/game_engine/ai/AStart.java
@@ -22,18 +22,11 @@ public class AStart {
     };
 
     public static void main(String[] args) {
-        //init
-
-        Pair<Integer, Integer> playerLoc = new Pair<Integer, Integer>(0, 1); //x and y
+        Pair<Integer, Integer> playerLoc = new Pair<Integer, Integer>(0, 1); //y and x
         Pair<Integer, Integer> enemLoc = new Pair<Integer, Integer>(5, 5);
 
-        realDist = calcRealDist(playerLoc);
-
         ArrayList<Pair<Integer, Integer>> path = aStar(enemLoc, playerLoc);
-        path  = removeLeafs(path);
 
-
-        System.out.println("\n\n\n");
         for (Pair<Integer, Integer> pair: path) {
             System.out.println(pair);
         }
@@ -43,71 +36,73 @@ public class AStart {
         Pair<Integer,Integer> biggestDist = path.get(0);
         Pair<Integer,Integer> peak;
 
-        System.out.println("\n\n");
-
+        // Find a node that has the biggest distance to the final end node.
+        // At this point realDist array still have values for the last A* search
         for (Pair<Integer, Integer> coord : path) {
-            System.out.println("Coords: " + coord + " Dist to player: " + realDist[coord.getKey()][coord.getValue()]);
             if(realDist[biggestDist.getKey()][biggestDist.getValue()] < realDist[coord.getKey()][coord.getValue()]){
                 biggestDist = coord;
             }
         }
 
-        peak = biggestDist;
-        System.out.println("Peak: " + peak);
+        // If the path is not straight to the end node, try to find shortcuts for it
+        if(biggestDist != path.get(0)){
+            ArrayList<Pair<Integer, Integer>> shortCut = aStar(path.get(0), biggestDist);
 
-        if(peak != path.get(0)){
-            System.out.println("ShortCut: ");
-            realDist = calcRealDist(peak);
-            ArrayList<Pair<Integer, Integer>> shortCut = aStar(path.get(0), peak);
-
-            for (Pair<Integer, Integer> coord : shortCut) {
-                System.out.println(coord);
-            }
             ArrayList<Pair<Integer, Integer>> shorterPath = new ArrayList<>();
 
+            // Add the starting "shortcut" node and the rest of the path
             for (int i = 0; i < shortCut.size(); i++) {
                 shorterPath.add(shortCut.get(i));
             }
-            shorterPath.addAll(path.subList(path.indexOf(peak) + 1, path.size()));
+            shorterPath.addAll(path.subList(path.indexOf(biggestDist) + 1, path.size()));
 
             return shorterPath;
         }
 
-
-
+        // If unable to find shortcuts, return the original path
         return path;
     }
 
     private static ArrayList<Pair<Integer, Integer>> aStar(Pair<Integer, Integer> startCoords, Pair<Integer, Integer> endCoords) {
-
-        ArrayList<Pair<Integer, Integer>> closed = new ArrayList();
+        // Straight line distances from every coords to end coords
+        realDist = calcRealDist(endCoords);
+        // Array list to store nodes after they have been expanded
+        ArrayList<Pair<Integer, Integer>> closed = new ArrayList<>();
+        // To store newly opened nodes
         PriorityQueue<Node> newNodes = null;
+        // To store every opened node
         PriorityQueue<Node> opened = openNodes(startCoords, 0d);
 
+        // You cannot expand start node
         closed.add(startCoords);
 
+        // A* finishes only when the end node is expanded
         while(!closed.contains(endCoords)) {
             newNodes = openNodes(opened.peek().getCoordinates(), opened.peek().getCostToGo());
+            // Add the coordinates of expanded node to the closed list and remove it from the opened queue
             closed.add(opened.poll().getCoordinates());
 
             for (Node n: newNodes) {
+                // Only add nodes to the open queue if they are not already there and the have not been expanded yet
                 if((!closed.contains(n.getCoordinates()) && (!opened.contains(n))))
                     opened.add(n);
             }
         }
 
+        // Shorten the path by finding shortcuts
+        closed  = removeLeafs(closed);
+
         return closed;
     }
 
     private static double[][] calcRealDist(Pair<Integer, Integer> endCoords) {
+        // Make a new equal sized 2D array
         double[][] realDist = new double[tiles.length][tiles[0].length];
 
-        System.out.println("\n\nReal (straight line) distances form every node to player: ");
+        // For every node, calculate the straight line distance to the end coords using Pythagoras theorem
         for (int i = 0; i < tiles.length; ++i) {
-            System.out.println();
             for (int j = 0; j < tiles[i].length; ++j) {
                 realDist[i][j] = sqrt(pow(j - endCoords.getValue(), 2) + pow(i - endCoords.getKey(), 2));
-                System.out.print(String.format("%.1f ", realDist[i][j]));
             }
         }
 
@@ -115,6 +110,7 @@ public class AStart {
     }
 
     private static PriorityQueue<Node> openNodes(Pair<Integer, Integer> nodeLoc, double costToGo) {
+        // Nodes in the PriorityQueue are ordered by costLeft + costToGo
         PriorityQueue<Node> initNodes = new PriorityQueue<>(8, (Node o1, Node o2) -> {
             if (o1.getSum() < o2.getSum())
                 return -1;
@@ -127,6 +123,7 @@ public class AStart {
         int topNodes = nodeLoc.getKey() - 1;
         int leftNodes = nodeLoc.getValue() - 1;
 
+        // Try to add nodes around the given node if they are not walls
         for (int i = topNodes; i < topNodes + 3; i++) {
             for (int j = leftNodes; j < leftNodes + 3; j++) {
                 try {
@@ -134,6 +131,7 @@ public class AStart {
                         initNodes.add(new Node(new Pair<>(i, j), costToGo + COST_OF_TRAVEL, realDist[i][j]));
                     }
                 } catch (Exception e) {
+                    // Will catch an exception if it tries to look for nodes that are outside the map bounds
                 }
             }
         }
