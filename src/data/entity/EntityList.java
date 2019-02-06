f8ecfe69e44af52902cf10f079f10d570306534f
dominik.rys@hotmail.com <dominik.rys@hotmail.com>
2019-02-06 21:37:25 +0000

    Fixed entity graphic path names

--- src/data/entity/EntityList.java
+++ src/data/entity/EntityList.java
@@ -2,20 +2,20 @@ package data.entity;
 
 public enum EntityList {
     // Item
-    PISTOL("assets/img/items/pistol.png"),
+    PISTOL("assets/img/entity/item/pistol.png"),
 
     // Player
-    PLAYER("assets/img/mobs/player_1.png"),
-    PLAYER_1("assets/img/mobs/player_1.png"),
-    PLAYER_2("assets/img/mobs/player_2.png"),
-    PLAYER_3("assets/img/mobs/player_3.png"),
-    PLAYER_4("assets/img/mobs/player_4.png"),
+    PLAYER("assets/img/entity/player/player_1.png"),
+    PLAYER_1("assets/img/entity/player/player_1.png"),
+    PLAYER_2("assets/img/entity/player/player_2.png"),
+    PLAYER_3("assets/img/entity/player/player_3.png"),
+    PLAYER_4("assets/img/entity/player/player_4.png"),
 
     // Enemy
-    ZOMBIE("assets/img/mobs/zombie.png"),
+    ZOMBIE("assets/img/entity/enemy/zombie.png"),
 
     // Projectile
-    BASIC_BULLET("assets/img/projectiles/basic_bullet.png"),
+    BASIC_BULLET("assets/img/entity/projectile/basic_bullet.png"),
 
     // Tiles
     GRASS_TILE("assets/img/tiles/grass.png"),
