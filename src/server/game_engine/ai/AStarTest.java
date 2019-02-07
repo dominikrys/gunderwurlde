a7b5b657efd0af8b7917c9cd569443c2015ff292
Tomvyc <tomas.vycas111@gmail.com>
2019-02-07 14:58:18 +0000

    Fix AStar tests

--- src/server/game_engine/ai/AStarTest.java
+++ src/server/game_engine/ai/AStarTest.java
@@ -1,5 +1,6 @@
 package server.game_engine.ai;
 
+import data.map.Meadow;
 import data.map.tile.Tile;
 import data.map.tile.TileState;
 import data.map.tile.TileTypes;
@@ -9,19 +10,15 @@ import java.util.ArrayList;
 
 public class AStarTest {
     public static void main(String[] args) {
-        final int[][] tiles = {
-                {1, 1, 0, 0, 0, 0, 1, 0, 0},
-                {0, 1, 1, 0, 0, 1, 1, 0, 0},
-                {0, 0, 1, 0, 0, 1, 1, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 1, 1, 1, 1, 1, 0, 0},
-                {0, 0, 1, 0, 0, 0, 0, 0, 0}
-        };
+
+        Meadow map = new Meadow();
+        final Tile[][] tiles = map.getTileMap();
+
 
         final double COST_OF_TRAVEL = 0.8;
 
         Pair<Integer, Integer> playerLoc = new Pair<>(5, 3); //y and x
-        Pair<Integer, Integer> enemLoc = new Pair<>(5, 1);
+        Pair<Integer, Integer> enemLoc = new Pair<>(25, 15);
         AStar aStar = new AStar(COST_OF_TRAVEL, tiles);
 
         ArrayList<Pair<Integer, Integer>> path = aStar.aStar(enemLoc, playerLoc);
@@ -32,9 +29,9 @@ public class AStarTest {
 //        TileTypes tileType = TileTypes.GRASS;
 //        TileState tileState = TileState.PASSABLE;
 
-        Tile tile1 = new Tile(TileTypes.GRASS, TileState.PASSABLE);
-        Tile tile2 = new Tile(TileTypes.WOOD, TileState.PASSABLE);
-
-        System.out.println(tile1.getState() == tile2.getState());
+//        Tile tile1 = new Tile(TileTypes.GRASS, TileState.PASSABLE);
+//        Tile tile2 = new Tile(TileTypes.WOOD, TileState.PASSABLE);
+//
+//        System.out.println(tile1.getState() == tile2.getState());
     }
 }
