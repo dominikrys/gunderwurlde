c05c80294da44fe3bb6eb8bb3aae1cb63fcb675a
rxm747 <rxm747@cs.bham.ac.uk>
2019-02-12 15:32:21 +0000

    changed ammo val

--- src/server/engine/state/entity/player/Player.java
+++ src/server/engine/state/entity/player/Player.java
@@ -49,7 +49,7 @@ public class Player extends Entity implements HasHealth, IsMovable, HasID {
         changeScore(team, DEFAULT_SCORE);
         this.name = name;
         this.ammo = new LinkedHashMap<>();
-        this.ammo.put(AmmoList.BASIC_AMMO, Integer.MAX_VALUE); // TODO remove after testing is finished
+        this.ammo.put(AmmoList.BASIC_AMMO, 120);
         this.playerID = nextPlayerID++;
     }
 
