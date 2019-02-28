410126139c5f1f26f468fc85d06499318cf1733d
dominik.rys@hotmail.com <dominik.rys@hotmail.com>
2019-02-28 16:05:52 +0000

    added another hardcoded controller

--- src/client/net/Addressing.java
+++ src/client/net/Addressing.java
@@ -23,7 +23,8 @@ public class Addressing {
                 while (addresses.hasMoreElements()) {
                     System.out.println(iface.getDisplayName());
                     InetAddress addr = addresses.nextElement();
-                    if(iface.getDisplayName().equals("Realtek PCIe GBE Family Controller") ){
+                    if (iface.getDisplayName().equals("Realtek PCIe GBE Family Controller") ||
+                            iface.getDisplayName().equals("Realtek Gaming GbE Family Controller")) {
                         System.out.println("Setting interface");
                         listenSocket.setInterface(addr);
                     }
