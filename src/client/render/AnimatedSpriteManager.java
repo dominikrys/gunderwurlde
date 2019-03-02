e0e18a57bd1bcf4aae117b4497c7b4c965ea6945
dominik.rys@hotmail.com <dominik.rys@hotmail.com>
2019-03-02 13:48:51 +0000

    Finished animation sprite manager class?

--- src/client/render/AnimatedSpriteManager.java
+++ src/client/render/AnimatedSpriteManager.java
@@ -7,44 +7,86 @@ import javafx.util.Duration;
 
 public class AnimatedSpriteManager {
     private Image image;
-    private int individualImageHeight;
-    private int individualImageWidth;
-    private int imageCount;
+    private int frameCount;
+    private int currentFrame;
     private int timeBetweenFrames;
     private Timeline timeline;
 
-    AnimatedSpriteManager(Image image, int individualImageHeight, int individualImageWidth, int imageCount, int timeBetweenFrames) {
+    private int sx;
+    private int sy;
+    private int individualImageWidth;
+    private int individualImageHeight;
+
+    /*
+    For use with GraphicsContext::drawImage()
+
+    Parameters:
+    img - the image to be drawn or null.
+    sx - the source rectangle's X coordinate position.
+    sy - the source rectangle's Y coordinate position.
+    sw - the source rectangle's width.
+    sh - the source rectangle's height.
+    dx - the destination rectangle's X coordinate position.
+    dy - the destination rectangle's Y coordinate position.
+    dw - the destination rectangle's width.
+    dh - the destination rectangle's height.
+     */
+
+    AnimatedSpriteManager(Image image, int individualImageHeight, int individualImageWidth, int frameCount, int timeBetweenFrames) {
         this.image = image;
         this.individualImageHeight = individualImageHeight;
         this.individualImageWidth = individualImageWidth;
-        this.imageCount = imageCount;
+        this.frameCount = frameCount;
         this.timeBetweenFrames = timeBetweenFrames;
+        this.sy = 0;
+        this.currentFrame = 0;
 
-        if (imageCount != 1) {
+        if (frameCount != 1) {
             timeline = new Timeline(new KeyFrame(Duration.millis(timeBetweenFrames),
                     e -> {
+                        if (currentFrame < frameCount) {
+                            sx += individualImageWidth;
+                            currentFrame++;
+                        } else {
+                            sx = 0;
+                            currentFrame = 0;
+                        }
 
                     }));
-
-            timeline.setCycleCount(imageCount);
+            timeline.setCycleCount(Timeline.INDEFINITE);
             timeline.play();
         }
     }
 
     AnimatedSpriteManager(Image image) {
         this.image = image;
-        this.individualImageHeight = 0;
-        this.individualImageWidth = 0;
-        this.imageCount = 1;
+        this.individualImageHeight = (int) image.getHeight();
+        this.individualImageWidth = (int) image.getWidth();
+        this.frameCount = 1;
         this.timeBetweenFrames = 0;
+        this.currentFrame = 0;
         this.timeline = new Timeline();
+        this.sx = 0;
+        this.sy = 0;
     }
 
-    Image getCurrentImage() {
-        if (imageCount == 1) {
-            return image;
-        } else {
+    public Image getImage() {
+        return image;
+    }
 
-        }
+    public int getSx() {
+        return sx;
+    }
+
+    public int getSy() {
+        return sy;
+    }
+
+    public int getImageWidth() {
+        return individualImageWidth;
+    }
+
+    public int getImageHeight() {
+        return individualImageHeight;
     }
 }
