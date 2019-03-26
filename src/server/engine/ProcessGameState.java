800bd656b17a49b37f9893fd60d9efbefec3ff5f
rxm747 <rxm747@cs.bham.ac.uk>
2019-03-26 20:35:17 +0000

    Fix engine not accepting LaserGuns.

--- src/server/engine/ProcessGameState.java
+++ src/server/engine/ProcessGameState.java
@@ -28,6 +28,7 @@ import server.engine.state.item.CreatesProjectiles;
 import server.engine.state.item.Item;
 import server.engine.state.item.consumable.Consumable;
 import server.engine.state.item.pickup.Health;
+import server.engine.state.item.weapon.gun.Gun;
 import server.engine.state.item.weapon.gun.ProjectileGun;
 import server.engine.state.laser.Laser;
 import server.engine.state.map.GameMap;
@@ -283,8 +284,8 @@ public class ProcessGameState extends Thread {
                     currentPlayer = (Player) currentPlayer.getEffect().applyEffect(currentPlayer);
 
                 // gun reload processing
-                if (currentItem instanceof ProjectileGun) {
-                    ProjectileGun currentGun = ((ProjectileGun) currentItem);
+                if (currentItem instanceof Gun) {
+                    Gun currentGun = ((Gun) currentItem);
                     if (currentGun.isReloading()) {
                         AmmoList ammoType = currentGun.getAmmoType();
                         int amountTaken = currentGun.reload(currentPlayer.getAmmo(ammoType));
@@ -299,13 +300,17 @@ public class ProcessGameState extends Thread {
                 long useTime = currentPlayer.getLastUseTime();
                 if (request.getShoot() && useTime < lastProcessTime) {
                     if (currentItem.getItemType() == ItemType.GUN) {
-                        ProjectileGun currentGun = (ProjectileGun) currentItem;
+                        Gun currentGun = (Gun) currentItem;
                         if (currentGun.shoot(currentPlayer.getAmmo(currentGun.getAmmoType()))) {
                             currentPlayer.setCurrentAction(ActionList.ATTACKING);
-                            LinkedList<Projectile> shotProjectiles = currentGun.getProjectiles(playerPose, currentPlayer.getTeam());
-                            for (Projectile p : shotProjectiles) {
-                                newProjectiles.add(p);
-                                projectilesView.add(new ProjectileView(p.getPose(), p.getSize(), p.getEntityListName(), p.isCloaked(), p.getStatus()));
+                            if (currentGun instanceof ProjectileGun) {
+                                LinkedList<Projectile> shotProjectiles = ((ProjectileGun) currentGun).getProjectiles(playerPose, currentPlayer.getTeam());
+                                for (Projectile p : shotProjectiles) {
+                                    newProjectiles.add(p);
+                                    projectilesView.add(new ProjectileView(p.getPose(), p.getSize(), p.getEntityListName(), p.isCloaked(), p.getStatus()));
+                                }
+                            } else {
+                                // TODO add lasers from laser gun
                             }
                         }
                     } else if (currentItem.getItemType() == ItemType.CONSUMEABLE
@@ -352,8 +357,8 @@ public class ProcessGameState extends Thread {
                     }
 
                 } else if (request.getReload()) {
-                    if (currentItem instanceof ProjectileGun) {
-                        ProjectileGun currentGun = ((ProjectileGun) currentItem);
+                    if (currentItem instanceof Gun) {
+                        Gun currentGun = ((Gun) currentItem);
                         if (currentGun.attemptReload(currentPlayer.getAmmo(currentGun.getAmmoType())))
                             currentPlayer.setCurrentAction(ActionList.RELOADING);
                         currentPlayer.setCurrentItem(currentItem);
@@ -948,7 +953,7 @@ public class ProcessGameState extends Thread {
         ArrayList<ItemView> playerItems = new ArrayList<>();
         for (Item i : p.getItems()) {
             if (i.getItemType() == ItemType.GUN) {
-                ProjectileGun g = (ProjectileGun) i;
+                Gun g = (Gun) i;
                 playerItems.add(new GunView(g.getItemListName(), g.getAmmoType(), g.getClipSize(), g.getAmmoInClip(), g.isAutoFire(), g.getReloadTime()));
             } else {
                 playerItems.add(new ItemView(i.getItemListName(), i.getItemType()));
