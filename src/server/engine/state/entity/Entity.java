cc01781181cab54bc85c6e86a3bed190535d1286
rxm747 <rxm747@cs.bham.ac.uk>
2019-03-29 12:41:16 +0000

    Entity javadoc.

--- src/server/engine/state/entity/Entity.java
+++ src/server/engine/state/entity/Entity.java
@@ -9,17 +9,50 @@ import shared.Pose;
 import shared.lists.EntityList;
 import shared.lists.EntityStatus;
 
-// Class for renderable entities
+/**
+ * Class for all Entities.
+ * 
+ * @author Richard
+ *
+ */
 public abstract class Entity {
+    /**
+     * Maximum size an entity can take.
+     */
     public static final int MAX_SIZE = (3 * Tile.TILE_SIZE);
-    
+    /**
+     * Pose of the entity (Location and direction)
+     */
     protected Pose pose;
+    /**
+     * The name, size and appearance to use for the entity.
+     */
     protected EntityList entityListName;
+    /**
+     * The status of the Entity.
+     */
     protected EntityStatus status;
+    /**
+     * Entity size as it's radius.
+     */
     protected int size;
+    /**
+     * Boolean to indicate if the entity is cloaked
+     */
     protected boolean cloaked; // "invisible"
+    /**
+     * The zoneID corresponding to the Zone that spawned the Entity.
+     */
     protected int zoneID;
 
+    /**
+     * Super Constructor that creates an Entity for the given pose, size &
+     * entityListName.
+     * 
+     * @param pose
+     * @param size
+     * @param entityListName
+     */
     protected Entity(Pose pose, int size, EntityList entityListName) {
         this.pose = pose;
         this.size = size;
@@ -29,64 +62,125 @@ public abstract class Entity {
         this.zoneID = -1;
     }
     
+    /**
+     * Super Constructor to create a template Entity for Spawning.
+     * 
+     * @param size
+     * @param entityListName
+     */
     protected Entity(int size, EntityList entityListName) {
         this (new Pose(), size, entityListName);
     }
 
+    /**
+     * Super Constructor to create an Entity with default direction.
+     * 
+     * @param location
+     * @param size
+     * @param entityListName
+     */
     protected Entity(Location location, int size, EntityList entityListName) {
         this(new Pose(location), size, entityListName);
     }
 
+    /**
+     * Abstract method to make a copy of the Entity.
+     * 
+     * @return A copy of the Entity.
+     */
     public abstract Entity makeCopy();
 
+    // getters and setters.
+
+    /**
+     * @return zoneID
+     */
     public int getZoneID() {
         return zoneID;
     }
 
+    /**
+     * @param zoneID
+     */
     public void setZoneID(int zoneID) {
         this.zoneID = zoneID;
     }
 
+    /**
+     * @return true if Cloaked
+     */
     public boolean isCloaked() {
         return cloaked;
     }
 
+    /**
+     * @param cloaked
+     */
     public void setCloaked(boolean cloaked) {
         this.cloaked = cloaked;
     }
 
+    /**
+     * @return Satus of the Entity
+     */
     public EntityStatus getStatus() {
         return status;
     }
 
+    /**
+     * @param status
+     */
     public void setStatus(EntityStatus status) {
         this.status = status;
     }
 
+    /**
+     * @return pose of the Entity
+     */
     public Pose getPose() {
         return pose;
     }
 
+    /**
+     * @return entityList value for that entity
+     */
     public EntityList getEntityListName() {
         return entityListName;
     }
 
+    /**
+     * @param pose
+     */
     public void setPose(Pose pose) {
         this.pose = pose;
     }
 
+    /**
+     * @return Location for the entity
+     */
     public Location getLocation() {
         return pose;
     }
 
+    /**
+     * @param location
+     */
     public void setLocation(Location location) {
         this.pose = new Pose(location, this.pose.getDirection());
     }
 
+    /**
+     * @return Size of the Entity (radius)
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Changes entity size with given amount.
+     * 
+     * @param amount
+     */
     public void changeSize(int amount) {
         if (amount <= -this.size) {
             this.size = 1;
@@ -97,6 +191,11 @@ public abstract class Entity {
         }
     }
 
+    /**
+     * Sets the Entity size to the given value.
+     * 
+     * @param size
+     */
     public void setSize(int size) {
         if (size <= 0) {
             this.size = 1;
@@ -107,6 +206,11 @@ public abstract class Entity {
         }
     }
     
+    /**
+     * Gets the Tiles the Entity is present on.
+     * 
+     * @return Collection of the TileCords for the Tiles that the Entity is on.
+     */
     public LinkedHashSet<int[]> getTilesOn() {
         Location loc = this.getLocation();
         int radius = size;
@@ -127,6 +231,13 @@ public abstract class Entity {
         return tilesOn;
     }
 
+    /**
+     * Checks if the Entity has collided with the given entity e2.
+     * 
+     * @param e2
+     *            Other entity to check with.
+     * @return true if they've collided.
+     */
     public boolean haveCollided(Entity e2) {
         Location e1_loc = this.getLocation();
         int e1_radius = this.size;
@@ -143,6 +254,13 @@ public abstract class Entity {
         return (dist_between_squared <= Math.pow(e1_radius + e2_radius, 2));
     }
 
+    /**
+     * Checks if the Entity has collided with the given laser l.
+     * 
+     * @param l
+     *            Laser to check with.
+     * @return true if they've collided.
+     */
     public boolean haveCollided(Laser l) {
         Location eLoc = this.getLocation();
         int eRadius = this.size;
